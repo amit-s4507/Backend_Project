944fa11c6f50bf4bd96529b40615f0a05e354c5b
Amit Sahu <amitsahu174507@gmail.com>
2025-06-22 00:31:16 +0530

    Added utils and ApiError

--- src/index.js
+++ src/index.js
@@ -9,6 +9,21 @@ dotenv.config({
 })
 
 connectDB()
+.then(() => {
+
+    // app.on("error" , (error) =>{
+    //     console.log("Error :" , error);
+    //     throw error;
+    // })
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+
+    })
+})
+.catch((err) => {
+    console.log("Mongo db connection failed !!!! ",err);
+
+})
 
 
 

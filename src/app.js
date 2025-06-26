67ba84a191cf54da1e9c628d021bcee340ebdaf1
Amit Sahu <amitsahu174507@gmail.com>
2025-06-27 01:54:07 +0530

    Done all the backend models , controllers and routes part

--- src/app.js
+++ src/app.js
@@ -1,34 +1,66 @@
 import express from "express"
 import cors from "cors"
-
 import cookieParser from "cookie-parser"
-
 
 const app = express()
 
-app.use(cors({
-     origin : process.env.CORS_ORIGIN,
-     credentials : true
-}))
+// check this middleware beacause the cokkie will not set
+// app.use(cors())
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend's origin
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', 'true'); // Important if using cookies or authentication
+//     next();
+// });
+
+let corsorigin={
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: "GET, POST, PUT, DELETE",
+}
 
 
-app.use(express.json({limit : "16kb"}))
-app.use(express.urlencoded({extended : true,limit : "16kb" }))
 
+
+app.use(cors(corsorigin))
+
+
+
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+console.log("app.js----", process.env.CORS_ORIGIN)
 
 
 
-// routes import
 
+//routes import
 import userRouter from './routes/user.routes.js'
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRouter from "./routes/video.routes.js"
+import commentRouter from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import aboutRouter from "./routes/about.routes.js"
 
-//routes declaration 
-// app.use("/users" , userRouter)
+//routes declaration
+// http://localhost:9000/api/v1/
 
-app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/user", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+app.use("/api/v1/about", aboutRouter);
 
-//http://localhost:8000/api/v1/users/register
-
-export { app };
+export { app }
